8b1fe62cc30920cb13ec324da014807e0f9ef8bb
RubenCG1997 <camachogomez1597@gmail.com>
2024-10-05 13:37:27 +0200

    Optimizacion de código

--- app/src/main/java/com/iescamas/calculadora/MainActivity.java
+++ app/src/main/java/com/iescamas/calculadora/MainActivity.java
@@ -1,11 +1,14 @@
 package com.iescamas.calculadora;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +17,6 @@ public class MainActivity extends AppCompatActivity {
     EditText txt_primernumero;
     EditText txt_segundonumero;
     TextView lbl_resultado;
-    Button btn_suma;
-    Button btn_resta;
-    Button btn_multiplicacion;
-    Button btn_division;
-    Button btn_borrar;
-
     //Mensajes
     final String msgError = "ERROR";
 
@@ -31,54 +28,57 @@ public class MainActivity extends AppCompatActivity {
         txt_primernumero = findViewById(R.id.etv_PrimerNumero);
         txt_segundonumero = findViewById(R.id.etv_SegundoNumero);
         lbl_resultado = findViewById(R.id.txt_Resultado);
-        btn_suma = findViewById(R.id.btn_Suma);
-        btn_resta = findViewById(R.id.btn_Resta);
-        btn_multiplicacion = findViewById(R.id.btn_Multiplicacion);
-        btn_division = findViewById(R.id.btn_Division);
-        btn_borrar = findViewById(R.id.btn_Borrar);
 
         //Se establecen los listener
-        btn_suma.setOnClickListener(view -> Calculadora('+'));
-        btn_resta.setOnClickListener(view -> Calculadora('-'));
-        btn_multiplicacion.setOnClickListener(view -> Calculadora('x'));
-        btn_division.setOnClickListener(view -> Calculadora('/'));
+        findViewById(R.id.btn_Suma).setOnClickListener(view -> Calculadora(1));
+        findViewById(R.id.btn_Resta).setOnClickListener(view -> Calculadora(2));
+        findViewById(R.id.btn_Multiplicacion).setOnClickListener(view -> Calculadora(3));
+        findViewById(R.id.btn_Division).setOnClickListener(view -> Calculadora(4));
         //Al realizar 3 eventos se pone de esta forma
-        btn_borrar.setOnClickListener(view -> {
+        findViewById(R.id.btn_Borrar).setOnClickListener(view -> {
             lbl_resultado.setText("");
             txt_primernumero.setText("");
             txt_segundonumero.setText("");
         });
 
     }
-    private void Calculadora(char operacion){
+    private int suma(int numero1,int numero2){
+
+        return numero1 + numero2;
+    }
+    private int resta (int numero1,int numero2){
+
+        return numero1 - numero2;
+    }
+    private int multiplicacion (int numero1,int numero2){
+
+        return numero1 * numero2;
+    }
+    private double division(double numero1,int numero2){
+
+        return (numero1/numero2);
+    }
+    @SuppressLint("DefaultLocale")
+    private void Calculadora(int operacion){
         //Obtenemos los textos de los editText y los convertimos en String
         String str_NumeroUno = txt_primernumero.getText().toString();
         String str_NumeroDos = txt_segundonumero.getText().toString();
+
         //Comprobamos que ninguno de los 2 este vacio
         if(!str_NumeroUno.isEmpty() && !str_NumeroDos.isEmpty()){
-            //Convertimos el String en Double
-            double numeroUno = Double.parseDouble(str_NumeroUno);
-            double numeroDos = Double.parseDouble(str_NumeroDos);
-            //Guardamos una variable para el resultado
-            double resultado;
+
+            int numero1 = Integer.parseInt(str_NumeroUno);
+            int numero2 = Integer.parseInt(str_NumeroDos);
             //Realizamos las operaciones
             switch(operacion){
-
-                case '+': resultado = numeroUno + numeroDos;
-                    lbl_resultado.setText(String.valueOf(resultado));
-                break;
-                case '-': resultado = numeroUno - numeroDos;
-                    lbl_resultado.setText(String.valueOf(resultado));
-                break;
-                case 'x': resultado = numeroUno * numeroDos;
-                    lbl_resultado.setText(String.valueOf(resultado));
-                break;
-                case '/': if(numeroDos==0){
+                case 1:lbl_resultado.setText(String.valueOf(suma(numero1,numero2)));break;
+                case 2:lbl_resultado.setText(String.valueOf(resta(numero1,numero2)));break;
+                case 3:lbl_resultado.setText(String.valueOf(multiplicacion(numero1,numero2)));break;
+                case 4: if(str_NumeroDos.equals("0")){
                     lbl_resultado.setText(msgError);
                 }
                 else{
-                    resultado = numeroUno / numeroDos;
-                    lbl_resultado.setText(String.valueOf(resultado));
+                    lbl_resultado.setText(String.format("%.2f",division(numero1,numero2)));
                 }
                 break;
             }
@@ -87,5 +87,4 @@ public class MainActivity extends AppCompatActivity {
             lbl_resultado.setText(msgError);
         }
         }
-
 }
